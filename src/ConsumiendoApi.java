216c96d0466db27165f055b51fd7bf0ee1ce6b43
haroldo capillo <haroldocapillo@hotmail.com>
2024-05-06 22:54:29 -0500

    challenge

---
+++ src/ConsumiendoApi.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/83120ee748c1255c8aa7078a/latest/USD"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
